aed2a7105fb7a25c74f1b3f8c629f66d7b4a674d
huyifan <yifan_hu@zju.edu.cn>
2020-07-17 20:49:14 +0800

    fxibug: update contract

--- gfl/io/eth/contract.py
+++ gfl/io/eth/contract.py
@@ -88,11 +88,76 @@ class ControllerContract(Contract):
     def __init__(self):
         super(ControllerContract, self).__init__()
 
-    def controll(self, args=()): # Job
-        self.call("controll", ())
+    def set_job_mapping_storage_contract(self, job_id: str, storage_contract_address: Address) -> Any:
+        """
+
+        :param job_id:
+        :param storage_contract_address:
+        :return:
+        """
+        self.sync_transact("SetJobMappingStorageContract", [job_id, storage_contract_address])
+
+    def get_job_mapping_storage_contract(self, job_id: str) -> Address:
+        return self.call("GetJobMappingStorageContract", [job_id])
+
+    def set_job_status(self, job_id: str, status: int) -> Any:
+        """
+
+        :param job_id:
+        :param status: 0->prepared, 1->running, 2->finished
+        :return:
+        """
+        self.sync_transact("SetJobStatus", [job_id, status])
+
+    def get_job_status(self, job_id: str) -> int:
+        self.call("GetJobStatus", [job_id])
+
 
 
 class StorageContract(Contract):
 
     def __init__(self):
         super(StorageContract, self).__init__()
+
+    def get_fed_step(self) -> int:
+        return self.call("GetFedStep")
+
+    def update_activate_clients(self, client_address: Address, status: int) -> Any:
+        """
+
+        :param client_id:
+        :param status: 0->join, 1->exit
+        :return:
+        """
+        self.sync_transact("UpdateActivateClients", [client_address, status])
+
+    def upload_model_parameters(self, fed_step: int, client_address: Address, model_pars_ipfs_hash: str) -> Any:
+        return self.sync_transact("UploadModelParameters", [fed_step, client_address, model_pars_ipfs_hash])
+
+    def selection_client_address(self, fed_step: int) -> Any:
+        return self.sync_transact("SelectionClientAddress", [fed_step])
+
+    def get_selection_client_address(self, fed_step) -> Address:
+        return self.call("GetSelectionClientAddress", [fed_step])
+
+    def download_model_parameters_ipfs_hash(self, fed_step: int, client_address: Address) -> Any:
+        return self.call("DownloadModelParametersIpfsHash", [fed_step, client_address])
+
+    def upload_aggregated_parameters_ipfs_hash(self, fed_step: int, client_address: Address, aggregated_model_pars_ipfs_hash: str) -> Any:
+        return self.sync_transact("UploadAggreagtedParametersIpfsHash", [fed_step, client_address, aggregated_model_pars_ipfs_hash])
+
+    def download_aggregated_parameters_ipfs_hash(self, fed_step: int) -> str:
+        return self.call("DownloadAggregatedParametersIpfsHash", [fed_step])
+
+    def download_final_aggregated_parameters_ipfs_hash(self) -> str:
+        return self.call("DownloadFinalAggregatedParametersIpfsHash")
+
+    def upload_final_aggregated_parameters_ipfs_hash(self, final_aggregated_ipfs_hash: str) -> Any:
+        return self.sync_transact("UploadFinalAggregatedParametersIpfsHash" [final_aggregated_ipfs_hash])
+
+
+
+
+
+
+
