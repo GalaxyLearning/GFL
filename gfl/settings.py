833a4f9390d7ff9062513004234a2dc6b92005fd
huyifan <yifan_hu@zju.edu.cn>
2020-07-22 11:04:35 +0800

    fixbug: update

--- gfl/settings.py
+++ gfl/settings.py
@@ -7,11 +7,7 @@ ipfs_url = "/dns/dev.malanore.cn/tcp/9013/http"
 
 DATA_DIR_PATH = "data"
 TEMP_DIR_PATH = os.path.join(tempfile.gettempdir(), "gfl")
-RUNTIME_CONFIG_SERVER_PATH = os.path.join(os.path.abspath("."), "runtime_config_server.json")
-RUNTIME_CONFIG_CLIENT_PATH = os.path.join(os.path.abspath("."), "runtime_config_server.json")
-JOB_CLIENT_DIR_PATH = os.path.join(os.path.abspath("."), "res", "jobs_client")
-JOB_SERVER_DIR_PATH = os.path.join(os.path.abspath("."), "res", "jobs_server")
-BASE_MODEL_DIR_PATH = os.path.join(os.path.abspath("."), "res", "models")
+
 
 g = globals().copy()
 
