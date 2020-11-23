0ddc50c6d8fad91ead89b7450dc1e28d0fe3e29a
huyifan <yifan_hu@zju.edu.cn>
2020-11-24 00:29:54 +0800

    update

--- examples/cifar10_distillation_demo/utils/data_split.py
+++ examples/cifar10_distillation_demo/utils/data_split.py
@@ -2,7 +2,7 @@
 from data_util import Data
 
 def main():
-    Data("cifar100", 2)
+    Data("femnist", 2)
 
 if __name__ == "__main__":
     main()
--- examples/cifar10_distillation_demo/utils/data_util.py
+++ examples/cifar10_distillation_demo/utils/data_util.py
@@ -4,7 +4,7 @@ from random import shuffle
 from torchvision.datasets import utils, MNIST, CIFAR10, CIFAR100
 from torchvision import transforms
 from torch.utils.data import Subset, DataLoader
-
+from PIL import Image
 
 class Data(object):
 
@@ -44,6 +44,65 @@ def data_split(dataset, split):
     splited = [Subset(dataset, splited_data[i]) for i in range(split)]
     return splited
 
+class FEMNIST(MNIST):
+    """
+    This dataset is derived from the Leaf repository
+    (https://github.com/TalwalkarLab/leaf) pre-processing of the Extended MNIST
+    dataset, grouping examples by writer. Details about Leaf were published in
+    "LEAF: A Benchmark for Federated Settings" https://arxiv.org/abs/1812.01097.
+    """
+    resources = [
+        ('https://raw.githubusercontent.com/tao-shen/FEMNIST_pytorch/master/femnist.tar.gz',
+         '59c65cec646fc57fe92d27d83afdf0ed')]
+
+    def __init__(self, root, train=True, transform=None, target_transform=None,
+                 download=False):
+        super(MNIST, self).__init__(root, transform=transform,
+                                    target_transform=target_transform)
+        self.train = train
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found.' +
+                               ' You can use download=True to download it')
+        if self.train:
+            data_file = self.training_file
+        else:
+            data_file = self.test_file
+
+        self.data, self.targets, self.users_index = torch.load(os.path.join(self.processed_folder, data_file))
+
+    def __getitem__(self, index):
+        img, target = self.data[index], int(self.targets[index])
+        img = Image.fromarray(img.numpy(), mode='F')
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return img, target
+
+    def download(self):
+        """Download the FEMNIST data if it doesn't exist in processed_folder already."""
+        import shutil
+
+        if self._check_exists():
+            return
+
+        utils.makedir_exist_ok(self.raw_folder)
+        utils.makedir_exist_ok(self.processed_folder)
+
+        # download files
+        for url, md5 in self.resources:
+            filename = url.rpartition('/')[2]
+            utils.download_and_extract_archive(url, download_root=self.raw_folder, filename=filename, md5=md5)
+
+        # process and save as torch files
+        print('Processing...')
+        shutil.move(os.path.join(self.raw_folder, self.training_file), self.processed_folder)
+        shutil.move(os.path.join(self.raw_folder, self.test_file), self.processed_folder)
+
 
 def Dataset(dataset):
     trainset, testset = None, None
@@ -81,9 +140,9 @@ def Dataset(dataset):
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,)),
         ])
-        # if args.dataset == 'femnist':
-        #     trainset = FEMNIST(root='~/data', train=True, transform=tra_trans, digitsonly=False)
-        #     testset = FEMNIST(root='~/data', train=False, transform=val_trans, digitsonly=False)
+        if dataset == 'femnist':
+            trainset = FEMNIST(root='~/data', train=True, transform=tra_trans, digitsonly=False)
+            testset = FEMNIST(root='~/data', train=False, transform=val_trans, digitsonly=False)
         if dataset == 'mnist':
             trainset = MNIST(root='~/data', train=True, transform=tra_trans)
             testset = MNIST(root='~/data', train=False, transform=val_trans)
