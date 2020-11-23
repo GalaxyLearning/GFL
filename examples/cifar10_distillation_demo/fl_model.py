70f4b8896cb41f1310806a41ff7ba4a08e07af8c
huyifan <yifan_hu@zju.edu.cn>
2020-11-23 23:04:50 +0800

    update

--- examples/cifar10_distillation_demo/fl_model.py
+++ examples/cifar10_distillation_demo/fl_model.py
@@ -22,6 +22,24 @@ class LeNet5(nn.Module):
         x = self.fc3(x)
         return x
 
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 64, 3)
+        self.fc1 = nn.Linear(64 * 4 * 4, 64)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 64 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 if __name__ == "__main__":
 
@@ -29,5 +47,5 @@ if __name__ == "__main__":
 
     job_manager = JobManager()
     job = job_manager.generate_job(work_mode=strategy.WorkModeStrategy.WORKMODE_STANDALONE,
-                                   fed_strategy=strategy.FederateStrategy.FED_DISTILLATION, epoch=50, model=LeNet5, distillation_alpha=0.5)
+                                   fed_strategy=strategy.FederateStrategy.FED_DISTILLATION, epoch=50, model=CNN, distillation_alpha=0.5)
     job_manager.submit_job(job, model)
