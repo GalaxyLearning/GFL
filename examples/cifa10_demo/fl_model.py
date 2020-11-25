d4cf8add760b70039f8aed8f7eeaf7ae775d75c8
huyifan <yifan_hu@zju.edu.cn>
2020-11-25 22:19:47 +0800

    update

--- examples/cifa10_demo/fl_model.py
+++ examples/cifa10_demo/fl_model.py
@@ -44,9 +44,9 @@ class CNN(nn.Module):
 
 if __name__ == "__main__":
 
-    model = LeNet5()
+    model = CNN()
 
     job_manager = JobManager()
     job = job_manager.generate_job(work_mode=strategy.WorkModeStrategy.WORKMODE_STANDALONE,
-                                   fed_strategy=strategy.FederateStrategy.FED_AVG, epoch=100, model=LeNet5)
+                                   fed_strategy=strategy.FederateStrategy.FED_AVG, epoch=100, model=CNN)
     job_manager.submit_job(job, model)
