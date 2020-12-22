2c78a8ee80df113d162e3cac0b4f75db9adb9912
huyifan <yifan_hu@zju.edu.cn>
2020-12-22 20:42:05 +0800

    update

---
+++ examples/cifar10_distillation_demo/utils/LDA_data_split_cifar100.py
@@ -0,0 +1,203 @@
+import os
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR100
+import numpy as np
+
+
+
+def record_net_data_stats(y_train, net_dataidx_map):
+    net_cls_counts = {}
+
+    for net_i, dataidx in net_dataidx_map.items():
+        unq, unq_cnt = np.unique(y_train[dataidx], return_counts=True)
+        tmp = {unq[i]: unq_cnt[i] for i in range(len(unq))}
+        net_cls_counts[net_i] = tmp
+    print('Data statistics: %s' % str(net_cls_counts))
+    return net_cls_counts
+
+class Cutout(object):
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, img):
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
+
+class CIFAR100_truncated(data.Dataset):
+
+    def __init__(self, root, dataidxs=None, train=True, transform=None, target_transform=None, download=False):
+
+        self.root = root
+        self.dataidxs = dataidxs
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+        self.download = download
+
+        self.data, self.target = self.__build_truncated_dataset__()
+
+    def __build_truncated_dataset__(self):
+        print("download = " + str(self.download))
+        cifar_dataobj = CIFAR100(self.root, self.train, self.transform, self.target_transform, self.download)
+
+        if self.train:
+            # print("train member of the class: {}".format(self.train))
+            # data = cifar_dataobj.train_data
+            data = cifar_dataobj.data
+            target = np.array(cifar_dataobj.targets)
+        else:
+            data = cifar_dataobj.data
+            target = np.array(cifar_dataobj.targets)
+
+        if self.dataidxs is not None:
+            data = data[self.dataidxs]
+            target = target[self.dataidxs]
+
+        return data, target
+
+    def truncate_channel(self, index):
+        for i in range(index.shape[0]):
+            gs_index = index[i]
+            self.data[gs_index, :, :, 1] = 0.0
+            self.data[gs_index, :, :, 2] = 0.0
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.target[index]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+def _data_transforms_cifar100():
+    CIFAR_MEAN = [0.5071, 0.4865, 0.4409]
+    CIFAR_STD = [0.2673, 0.2564, 0.2762]
+
+    train_transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+
+    train_transform.transforms.append(Cutout(16))
+
+    valid_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+
+    return train_transform, valid_transform
+
+def load_cifar10_data(datadir):
+    train_transform, test_transform = _data_transforms_cifar100()
+
+    cifar10_train_ds = CIFAR100_truncated(datadir, train=True, download=True, transform=train_transform)
+    cifar10_test_ds = CIFAR100_truncated(datadir, train=False, download=True, transform=test_transform)
+
+    X_train, y_train = cifar10_train_ds.data, cifar10_train_ds.target
+    X_test, y_test = cifar10_test_ds.data, cifar10_test_ds.target
+
+    return (X_train, y_train, X_test, y_test)
+
+def partition_data(dataset, datadir, partition, n_nets, alpha):
+    print("*********partition data***************")
+    X_train, y_train, X_test, y_test = load_cifar10_data(datadir)
+    n_train = X_train.shape[0]
+    # n_test = X_test.shape[0]
+
+    if partition == "homo":
+        total_num = n_train
+        idxs = np.random.permutation(total_num)
+        batch_idxs = np.array_split(idxs, n_nets)
+        net_dataidx_map = {i: batch_idxs[i] for i in range(n_nets)}
+
+    elif partition == "hetero":
+        min_size = 0
+        K = 100
+        N = y_train.shape[0]
+        print("N = " + str(N))
+        net_dataidx_map = {}
+
+        while min_size < 10:
+            idx_batch = [[] for _ in range(n_nets)]
+            # for each class in the dataset
+            for k in range(K):
+                idx_k = np.where(y_train == k)[0]
+                np.random.shuffle(idx_k)
+                proportions = np.random.dirichlet(np.repeat(alpha, n_nets))
+                ## Balance
+                # print(proportions.shape)
+                proportions = np.array([p * (len(idx_j) < N / n_nets) for p, idx_j in zip(proportions, idx_batch)])
+                # print(proportions.shape)
+                proportions = proportions / proportions.sum()
+                # print(proportions.shape)
+                proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
+                # print(proportions.shape)
+                idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
+                min_size = min([len(idx_j) for idx_j in idx_batch])
+
+        for j in range(n_nets):
+            np.random.shuffle(idx_batch[j])
+            net_dataidx_map[j] = idx_batch[j]
+
+    # elif partition == "hetero-fix":
+    #     dataidx_map_file_path = './data_preprocessing/non-iid-distribution/CIFAR10/net_dataidx_map.txt'
+    #     net_dataidx_map = read_net_dataidx_map(dataidx_map_file_path)
+    #
+    # if partition == "hetero-fix":
+    #     distribution_file_path = './data_preprocessing/non-iid-distribution/CIFAR10/distribution.txt'
+    #     traindata_cls_counts = read_data_distribution(distribution_file_path)
+    # else:
+        traindata_cls_counts = record_net_data_stats(y_train, net_dataidx_map)
+
+    obj_id = 0
+    while obj_id < n_nets:
+        new_x_train = torch.Tensor(X_train[net_dataidx_map.get(obj_id)])
+        new_y_train = torch.Tensor(y_train[net_dataidx_map.get(obj_id)])
+        new_x_train = new_x_train.permute(0, 3, 1, 2)
+        train_dataset = torch.utils.data.TensorDataset(new_x_train, new_y_train)
+        train_dataset_path = os.path.join("../data","train_dataset_{}".format(obj_id))
+        torch.save(train_dataset, train_dataset_path)
+        obj_id += 1
+    new_x_test = torch.Tensor(X_test)
+    new_x_test = new_x_test.permute(0, 3, 1, 2)
+    new_y_test = torch.Tensor(y_test)
+    test_dataset = torch.utils.data.TensorDataset(new_x_test, new_y_test)
+    test_dataset_path = os.path.join("../data", "test_dataset")
+    # print(test_dataset, test_dataset_path)
+    # test_dataset_path = os.path.join("../data", "test_dataset")
+    torch.save(test_dataset, test_dataset_path)
+    return X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts
+
+
+if __name__ == "__main__":
+    partition_data(None, "~/data", "hetero", 10, 0.5)
