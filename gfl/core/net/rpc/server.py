5208a55767dcc9deb7b97216b5ac58bb977fa0f8
malanore <malanore.z@gmail.com>
2022-07-02 18:56:11 +0800

    Zyh (#31)
    Revert LICENSE to Apache2

--- gfl/core/net/rpc/server.py
+++ gfl/core/net/rpc/server.py
@@ -1,14 +1,28 @@
+#  Copyright 2020 The GFL Authors. All Rights Reserved.
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from concurrent import futures
 
 import grpc
 
 import gfl.core.net.rpc.gfl_pb2_grpc as gfl_pb2_grpc
-from gfl.runtime.manager import NodeManager
+from gfl.runtime.manager.server_manager import ServerManager
 
 
 class GflServicer(gfl_pb2_grpc.GflServicer):
 
-    def __init__(self, manager: NodeManager):
+    def __init__(self, manager: ServerManager):
         super(GflServicer, self).__init__()
         self._manager = manager
 
---
+++ gfl/utils/sys_utils.py
@@ -0,0 +1,88 @@
+#  Copyright 2020 The GFL Authors. All Rights Reserved.
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import psutil
+
+
+class SysUtils(object):
+    """
+    Some methods of query the usage system hardware resources.
+    The following is meaning of some identifier names:
+
+    proc_*: query resources of specified process
+    pid: process id, None means current process
+    index: serial number of cpu or gpu, start from 0
+    """
+
+    @classmethod
+    def cpu_count(cls, logical=True):
+        return psutil.cpu_count(logical)
+
+    @classmethod
+    def cpu_percent(cls, index=None):
+        """
+        return the average used percent if index is None or less than 0
+        """
+        if index is None or index < 0:
+            return psutil.cpu_percent()
+        else:
+            return psutil.cpu_percent(percpu=True)[index]
+
+    @classmethod
+    def mem_total(cls):
+        pass
+
+    @classmethod
+    def mem_used(cls):
+        pass
+
+    @classmethod
+    def mem_available(cls):
+        pass
+
+    @classmethod
+    def mem_free(cls):
+        pass
+
+    @classmethod
+    def gpu_count(cls):
+        pass
+
+    @classmethod
+    def gpu_mem_total(cls, index):
+        pass
+
+    @classmethod
+    def gpu_mem_used(cls, index):
+        pass
+
+    @classmethod
+    def gpu_mem_free(cls, index):
+        pass
+
+    @classmethod
+    def gpu_utilization_rates(cls, index):
+        pass
+
+    @classmethod
+    def proc_cpu_percent(cls, pid=None):
+        pass
+
+    @classmethod
+    def proc_mem_used(cls, pid=None):
+        pass
+
+    @classmethod
+    def proc_gpu_mem_used(cls, pid=None, index=None):
+        pass
